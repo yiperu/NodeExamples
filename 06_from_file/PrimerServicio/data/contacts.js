83616b90d545096982567a2f82ec8ccf61c9dbc5
Henry AT <yiperu@gmail.com>
2016-06-18 03:45:58 -0400

    Creations od data for test

---
+++ 06_from_file/PrimerServicio/data/contacts.js
@@ -0,0 +1,12 @@
+{
+	"firstname": "Juan",
+	"lastname": "Blanco",
+	"title": "Sr.",
+	"company": "Reunidas Inc",
+	"jobtitle": "Desarrollador",
+	"primarycontactnumber": "+459777123",
+	"othercontactnumber": ["+4597774567","+459777112"],
+	"primaryemailaddress": "juan.blanco@abc.com",
+	"emailaddresses": ["j.blanco@abc.com"],
+	"groups": ["Des","Amigos"]
+}
